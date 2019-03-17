50e6d4c4bf9f5840b24155fad0a38093f3b6542f
felipe <felipe.meriga@gmail.com>
2019-03-17 10:25:17 -0300

    Backend - Criar lambda que devolve detalhes sobre um ponto

---
+++ aws/lambda/getThingSpeakCurrVal/lambda_function.py
@@ -0,0 +1,71 @@
+import json
+import urllib.request
+from datetime import datetime
+import pytz
+import math
+
+
+def lambda_handler(event, context):
+
+    try:
+        url = event['endPoint']
+        response = urllib.request.urlopen(url)
+        data = json.loads(response.read().decode(response.info().get_param('charset') or 'utf-8'))
+
+        date = datetime.strptime(data['feeds'][0]['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+        local = pytz.timezone('America/Sao_Paulo')
+
+        localDateTmz = date.astimezone(local)
+        localDate = localDateTmz + localDateTmz.utcoffset()
+
+        data['feeds'][0]['created_at'] = localDate.strftime("%Y-%m-%d %H:%M:%S")
+
+        temperature = float(data['feeds'][0]['field1'])
+        moisure = float(data['feeds'][0]['field2'])
+
+    except:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error getting values from ThingSpeak')
+        }
+
+    try:
+        if moisure > 100:
+            moisure = 100
+
+        temperature = ((temperature * 1.8) + 32)
+
+        thermalConfort = (0.5 * ((temperature + 61 + ((temperature - 68) * 1.2) + (moisure * 0.094))));
+
+        if (thermalConfort >= 80):
+            HI = (-42.379 + (2.04901523 * temperature) + (10.14333127 * moisure) -
+                  (0.22475541 * temperature * moisure) -
+                  (0.00683783 * temperature * temperature) -
+                  (0.05481717 * moisure * moisure) +
+                  (0.00122874 * temperature * temperature * moisure) +
+                  (0.00085282 * temperature * moisure * moisure) -
+                  (0.00000199 * temperature * temperature * moisure * moisure));
+            offset = 0;
+            thermalConfort = HI;
+            if ((moisure < 13) and (temperature >= 80 and temperature <= 112)):
+                offset = ((13 - moisure) / 4) * math.sqrt((17 - abs(temperature - 95.)) / 17);
+                thermalConfort = HI - AJUSTE;
+            if ((moisure > 85) and (temperature >= 80 and temperature <= 87)):
+                offset = ((moisure - 85) / 10) * ((87 - temperature) / 5);
+                thermalConfort = HI + offset;
+
+        thermalConfort = ((thermalConfort - 32) / 1.8);
+        data['feeds'][0]['thermalConfort'] = thermalConfort
+
+
+    except:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error durring calculating the Thermal Confort')
+        }
+
+    return {
+        'statusCode': 200,
+        'body': data['feeds']
+    }
+

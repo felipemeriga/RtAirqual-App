d7d5f31542c389a7a9f58a836ccac49d5601845a
felipe <felipe.meriga@gmail.com>
2019-03-21 10:51:32 -0300

    Modifying index.js over pure-components

--- src/components/pure-components/index.js
+++ src/components/pure-components/index.js
@@ -4,7 +4,7 @@ export {default as Styles} from "./Styles";
 export {default as Circle} from "./Circle";
 export {default as BaseContainer} from "./BaseContainer";
 export {default as Task} from "./Task";
-export {default as Images} from "./images";
+export {default as Images} from "../images";
 export {default as Avatar} from "./Avatar";
 export {default as TaskOverview} from "./TaskOverview";
 export {default as Field} from "./Field";

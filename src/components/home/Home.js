1dd3af945bc2e5e28597aec9d76c1852ac2cc252
unknown <susselguilherme@gmail.com>
2019-03-25 19:59:05 -0300

    mudancas (incompletas)no mapa

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,70 +1,70 @@
 // @flow
-import * as React from "react";
-import {StyleSheet, View} from "react-native";
-
-
-import {BaseContainer, Styles} from "../pure-components";
-import type {ScreenProps} from "../pure-components/Types";
-
-import {Marker} from "react-native-maps";
-import MapView from "react-native-maps";
-import { Constants, Location, Permissions } from 'expo';
+import { AppRegistry, StyleSheet, Dimensions, View } from "react-native";
 import { TabNavigator } from "react-navigation";
 import { Container, Text } from "native-base";
+import * as React from "react";
+import {BaseContainer, Styles} from "../pure-components";
+import type {ScreenProps} from "../pure-components/Types";
+import MapView from "react-native-maps";
+import { Callout } from "react-native-maps";
+
 
 export default class Home extends React.Component<ScreenProps<>> {
+    constructor(props) {
+        super(props);
 
-    go(key: string) {
-        this.props.navigation.navigate(key);
+        this.state = {
+            latitude: null,
+            longitude: null,
+            error: null
+        };
     }
-	
-	constructor(props){
-    super(props);
-    state = {
-        myLocation: null
-    };
-}
 
-componentDidMount = () => {
-    this.getMyLocation();
-};
-
-getMyLocation = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-        this.setState({
-            myLocation: 'Permission denied',
-        });
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log("wokeeey");
+                console.log(position);
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    error: null
+                });
+            },
+            (error) => this.setState({ error: error.message }),
+            { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+        );
     }
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({
-        myLocation: JSON.stringify(location)
-    });
-};
-	
-    render(): React.Node {
-        const {navigation} = this.props;
+
+
+    render() {
         const nomePagina = "rtAirQual";
+        const {navigation} = this.props;
         return (
-            <BaseContainer title={nomePagina} {...{navigation}} scrollable style={style.container}>
+            <BaseContainer title={nomePagina} {...{navigation}} scrollable style={styles.container}>
                 <View style={Styles.flexGrow}>
-                    <View style={[style.mapContainer, Styles.center]}>
-                        <MapView style={style.cardContainer}
-                                 initialRegion={{
-                                     latitude: -23.3436887,
-                                     longitude: -51.1726026,
-                                     latitudeDelta: 0.0922,
-                                     longitudeDelta: 0.0421,
-                                 }}>
+                    <View style={[styles.mapContainer, Styles.center]}>
+                        <MapView
+                            style={styles.cardContainer}
+                            initialRegion={{
+                                latitude: -23.3436887,
+                                longitude: -51.1726026,
+                                latitudeDelta: 1,
+                                longitudeDelta: 1
+                            }}
+                        >
+
                             <MapView.Marker
                                 coordinate={{
                                     latitude: -23.329533,
                                     longitude: -51.173548
                                 }}
-								image={{uri: 'https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png'}}
-                                title={"Lago 2"}
-                                description={"description"}
+                                image={{uri: "https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png"}}
+                                title="Lago 2"
+                                description="description"
+                                onpre
                             >
+
                             </MapView.Marker>
 
                             <MapView.Marker
@@ -72,9 +72,9 @@ getMyLocation = async () => {
                                     latitude: -23.322067,
                                     longitude: -51.173079
                                 }}
-								image={{uri: 'https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png'}}
-                                title={"Bosque Universitário"}
-                                description={"description"}
+                                image={{uri: "https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png"}}
+                                title="Bosque Universitário"
+                                description="description"
                             >
                             </MapView.Marker>
 
@@ -83,9 +83,9 @@ getMyLocation = async () => {
                                     latitude: -23.299477,
                                     longitude: -51.210220
                                 }}
-                                image={{uri: 'https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png'}}
-                                title={"PUC"}
-                                description={"description"}
+                                image={{uri: "https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png"}}
+                                title="PUC"
+                                description="description"
                             >
                             </MapView.Marker>
 
@@ -94,9 +94,9 @@ getMyLocation = async () => {
                                     latitude: -23.307478,
                                     longitude: -51.113860
                                 }}
-								image={{uri: 'https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png'}}
-                                title={"UTFPR"}
-                                description={"description"}
+                                image={{uri: "https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png"}}
+                                title="UTFPR"
+                                description="description"
                             >
                             </MapView.Marker>
 
@@ -105,9 +105,9 @@ getMyLocation = async () => {
                                     latitude: -23.323550,
                                     longitude: -51.163392
                                 }}
-								image={{uri: 'https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png'}}
-                                title={"Zerão"}
-                                description={"description"}
+                                image={{uri: "https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png"}}
+                                title="Zerão"
+                                description="description"
                             >
                             </MapView.Marker>
 
@@ -116,11 +116,16 @@ getMyLocation = async () => {
                                     latitude: -23.322218,
                                     longitude: -51.206351
                                 }}
-								image={{uri: 'https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png'}}
-                                title={"Pista Atletismo UEL"}
-                                description={"description"}
+                                image={{uri: "https://raw.githubusercontent.com/felipemeriga/RtAirqual-App/master/assets/marker_rt.png"}}
+                                title="Pista Atletismo UEL"
+                                description="description"
                             >
                             </MapView.Marker>
+
+                            {!!this.state.latitude && !!this.state.longitude && <MapView.Marker
+                                coordinate={{latitude: this.state.latitude, longitude: this.state.longitude}}
+                                title="Your Location"
+                            />}
                         </MapView>
                     </View>
                 </View>
@@ -129,7 +134,7 @@ getMyLocation = async () => {
     }
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1
     },

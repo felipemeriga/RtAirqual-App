07b8466a11ee68ddb0e9f3b877b8b1ecaa490e2e
felipe <felipe.meriga@gmail.com>
2019-03-29 20:06:34 -0300

    Frontend - Criar MapsStore para coletar datos de um marcador e abrir o dialog(modal)

--- src/stores/MapsStore.js
+++ src/stores/MapsStore.js
@@ -1,10 +1,35 @@
 // @flow
 import {observable, action} from "mobx";
-
+import axios from "axios";
 
 class MapsStore {
     @observable dialogOn = false;
     @observable loadingDetail = false;
+    @observable markDetail: Object;
+    @observable error = false;
+
+    @action
+    async getMarkDetail(mark: any): React.node {
+        this.dialogOn = true;
+        this.loadingDetail = true;
+        axios.get(mark.endPoint)
+            .then((response) => {
+                this.markDetail = response.data.body;
+                this.loadingDetail = false;
+            })
+            .catch((err) => {
+                console.log(err);
+                this.error = true;
+                this.loadingDetail = false;
+            });
+    }
+
+    @action
+    onTouchOutside(): React.node {
+        this.loadingDetail = false;
+        this.dialogOn = false;
+        this.error = false;
+    }
 
 
 }

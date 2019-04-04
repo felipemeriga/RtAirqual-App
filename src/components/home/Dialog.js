cae3ec3f8b133923c27317c498c816f83de48235
felipemeriga <felipe.meriga@gmail.com>
2019-04-04 11:49:17 -0300

    Frontend - Refactor do component de Dialog do mapa

--- src/components/home/Dialog.js
+++ src/components/home/Dialog.js
@@ -1,28 +1,15 @@
 // @flow
-
 import React from "react";
 import * as Progress from "react-native-progress";
 import Dialog, {SlideAnimation, DialogContent, DialogTitle} from "react-native-popup-dialog";
-import {inject, observer} from "mobx-react";
 import PropTypes from "prop-types";
-import MapView from "react-native-maps";
-import {View, StyleSheet, Text} from "react-native";
-import Images from "../images";
+import {StyleSheet, Text, View} from "react-native";
 import {Styles} from "../pure-components";
 
-@inject("mapsStore")
-@observer
-export default class Map extends React.Component<{}> {
 
-    onMarkerTouched(marker: any): React.Node {
-        this.props.mapsStore.getMarkDetail(marker);
-    }
+export default class Map extends React.PureComponent<{}> {
 
     getRenderContent(): React.Node {
-        return this.getMapView();
-    }
-
-    getDialog(): React.Node {
         return (
             <Dialog
                 dialogTitle={<DialogTitle title={this.props.mapsStore.marker.name} style={styles.titleDialog}/>}
@@ -93,72 +80,25 @@ export default class Map extends React.Component<{}> {
                 </DialogContent>
             </Dialog>
         );
-
     }
 
-
-    getMapView(): React.Node {
-        const channels = this.props.channels;
-        return (
-            <MapView
-                showsUserLocation
-                mapType="standard" // changes map style, default = standard, satellite, hybrid, terrain
-                style={styles.cardContainer}
-                initialRegion={{
-                    latitude: this.props.localization.latitude,
-                    longitude: this.props.localization.longitude,
-                    latitudeDelta: 0.04,
-                    longitudeDelta: 0.04
-                }}
-            >
-                {
-                    channels.map((member, index) => {
-                        return (<MapView.Marker
-                            key={index}
-                            coordinate={{
-                                latitude: member.latitude,
-                                longitude: member.longitude
-                            }}
-                            image={Images.rtMarker}
-                            onPress={() => {
-                                this.onMarkerTouched(member);
-                            }}
-                        >
-                        </MapView.Marker>);
-                    })
-                }
-                {this.getDialog()}
-            </MapView>
-        );
-    }
-
-    componentDidMount() {
-    }
 
     static get propTypes(): React.Node {
         return {
-            channels: PropTypes.arrayOf(Object),
-            localization: PropTypes.any,
-            mapsStore: PropTypes.any
+            mapsStore: PropTypes.arrayOf(Object)
         };
     }
 
+
     render(): React.Node {
         return (
-            <View style={Styles.flexGrow}>
-                <View style={[styles.mapContainer, Styles.center]}>
-                    {this.getRenderContent()}
-                </View>
+            <View>
+                {this.getRenderContent()}
             </View>
         );
     }
 }
 
-export const getCurrentLocation = () => {
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
-    });
-};
 
 const styles = StyleSheet.create({
     textNormal: {
@@ -209,12 +149,5 @@ const styles = StyleSheet.create({
     },
     hideLoadingDialog: {
         display: "none"
-    },
-    mapContainer: {
-        flex: 1
-    },
-    cardContainer: {
-        width: 500,
-        height: 1000
     }
 });

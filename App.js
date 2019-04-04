0d7cd88320062950274b8ea73acb0734fb63b511
felipemeriga <felipe.meriga@gmail.com>
2019-04-04 14:49:07 -0300

    Backend - Adicionar aws amplify ao projeto

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import * as React from "react";
 import {Dimensions} from "react-native";
 import {StyleProvider} from "native-base";
 import {Provider} from "mobx-react";
+import Amplify, { Auth } from "aws-amplify";
 import {
     createAppContainer, createSwitchNavigator, createDrawerNavigator
 } from "react-navigation";
@@ -103,5 +104,52 @@ const AppNavigator = createAppContainer(createSwitchNavigator({
         backgroundColor: variables.brandInfo
     }
 }));
+
+Amplify.configure({
+    Auth: {
+
+        // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+        identityPoolId: "us-west-2:dbd50025-b072-41d4-ace9-1785d737524e",
+
+        // REQUIRED - Amazon Cognito Region
+        region: "us-west-2",
+
+        // OPTIONAL - Amazon Cognito Federated Identity Pool Region
+        // Required only if it"s different from Amazon Cognito Region
+        identityPoolRegion: "us-west-2",
+
+        // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: "us-west-2_4OQ3MrZUf",
+
+        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+        userPoolWebClientId: "1fcijuj9fsvvtf8n5682s121ud",
+
+        // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+        mandatorySignIn: false,
+
+        // OPTIONAL - Configuration for cookie storage
+        // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+        cookieStorage: {
+            // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+            domain: ".yourdomain.com",
+            // OPTIONAL - Cookie path
+            path: "/",
+            // OPTIONAL - Cookie expiration in days
+            expires: 365,
+            // OPTIONAL - Cookie secure flag
+            // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+            secure: true
+        },
+
+        // OPTIONAL - customized storage object
+        // storage: new MyStorage(),
+
+        // OPTIONAL - Manually set the authentication flow type. Default is "USER_SRP_AUTH"
+        authenticationFlowType: "USER_PASSWORD_AUTH"
+    }
+});
+
+// You can get the current config object
+const currentConfig = Auth.configure();
 
 export {AppNavigator};
